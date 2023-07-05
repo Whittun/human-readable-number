3ab2749ccfb453e81875847e8d1b321c3946f913
Whittun <whittun@gmail.com>
2023-07-05 16:00:44 +0300

    feat: meet the challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function toReadable (number) {
-  
+    const textNumbers = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    const numbersInTens = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+    let textNumber;
+
+    number = number.toString();
+
+    if (+number === 0) {
+        textNumber = "zero";
+    } else if (+number < 20) {
+        textNumber = textNumbers[+number - 1]
+    } else if (+number < 100) {
+        textNumber = numbersInTens[+number[0] - 2]
+        if (+number[1] !== 0) {
+            textNumber += " " + toReadable(+number[1]);
+        }
+    } else if (+number < 1000) {
+        textNumber = textNumbers[+number[0] - 1] + " " + "hundred";
+
+        if (+number[1] !== 0 || +number[2] !== 0) {
+            textNumber += " " + toReadable(+(number[1] + number[2]))
+        }
+    } 
+        
+    return textNumber
 }
